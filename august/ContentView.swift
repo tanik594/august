0d49687e91d4d9c4e69072d8e60339a945c91f13
谷口幸路 <tanik@magictouch-inc.jp>
2020-08-05 08:46:03 +0900

    Initial Commit

---
+++ august/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  august
+//
+//  Created by 谷口幸路 on 2020/08/05.
+//  Copyright © 2020 谷口幸路. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
