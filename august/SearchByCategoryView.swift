c72311f484750abe3a7332aa6c2904056313bc1f
谷口幸路 <tanik@magictouch-inc.jp>
2020-08-19 19:49:56 +0900

    read csv

--- august/SearchByCategoryView.swift
+++ august/SearchByCategoryView.swift
@@ -9,8 +9,34 @@
 import SwiftUI
 
 struct SearchByCategoryView: View {
+    @State var csvLines = [String]()
+    
     var body: some View {
-        Text("分類から調べる")
+        VStack {
+            Text("分類から調べる")
+            
+        }
+        .onAppear(perform: {
+            guard let path = Bundle.main.path(forResource: "dataList", ofType: "csv") else {
+                print("csvファイルがないよ")
+                return
+            }
+            
+            do {
+                let csvString = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+                self.csvLines = csvString.components(separatedBy: .newlines)
+                self.csvLines.removeLast()
+            } catch let error as NSError {
+                print("Error: \(error)")
+                return
+            }
+            
+            for animalData in self.csvLines {
+                let animalDetail = animalData.components(separatedBy: ",")
+                print("【名前】\(animalDetail[0])　【体長】\(animalDetail[1]) cm　【体重】\(animalDetail[2]) kg")
+                
+            }
+        })
     }
 }
 
