cba0aad44a5de99968f2d0507278024202674e47
谷口幸路 <tanik@magictouch-inc.jp>
2020-08-26 19:12:10 +0900

    add List

--- august/SearchByCategoryView.swift
+++ august/SearchByCategoryView.swift
@@ -8,13 +8,28 @@
 
 import SwiftUI
 
+struct Animal: Identifiable {
+    let id: Int
+    let name: String
+    let height: String
+    let weight: String
+}
+
+
 struct SearchByCategoryView: View {
     @State var csvLines = [String]()
+    @State var csvArr = [Animal]()
     
     var body: some View {
         VStack {
             Text("分類から調べる")
-            
+            List(csvArr) {animal in
+                HStack {
+                    Text("\(animal.name)")
+                    Text("\(animal.height)")
+                    Text("\(animal.weight)")
+                }
+            }
         }
         .onAppear(perform: {
             guard let path = Bundle.main.path(forResource: "dataList", ofType: "csv") else {
@@ -31,10 +46,13 @@ struct SearchByCategoryView: View {
                 return
             }
             
+            var cnt = 0
             for animalData in self.csvLines {
                 let animalDetail = animalData.components(separatedBy: ",")
                 print("【名前】\(animalDetail[0])　【体長】\(animalDetail[1]) cm　【体重】\(animalDetail[2]) kg")
                 
+                self.csvArr.append(Animal(id: cnt, name: animalDetail[0], height: animalDetail[1], weight: animalDetail[2]))
+                cnt += 1
             }
         })
     }
