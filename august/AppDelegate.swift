6cbdd3980dfe5d6de74eaa6cfb2e9252c2166f9b
谷口幸路 <tanik@magictouch-inc.jp>
2021-02-16 19:33:15 +0900

    add local notification

--- august/AppDelegate.swift
+++ august/AppDelegate.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(
+        options: [.alert, .sound, .badge]){
+            (granted, _) in
+            if granted{
+                UNUserNotificationCenter.current().delegate = self
+            }
+        }
         return true
     }
 
--- august/NotificationView.swift
+++ august/NotificationView.swift
@@ -9,8 +9,46 @@
 import SwiftUI
 
 struct NotificationView: View {
+    
+    func setNotification() -> Void {
+        //日時を指定する
+        //let triggerDate = DateComponents(month:2, day:16, hour:18, minute:33, second: 0)
+        //let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        //曜日と時間を指定して繰り返す 3=火曜日
+        let triggerDate = DateComponents(hour:18, minute:56, weekday:3)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "ゴミカレンダー"
+        content.body = "明日は燃えるゴミの日です"
+        content.sound = UNNotificationSound.default
+        
+        let request = UNNotificationRequest.init(identifier: "CalendarNotification", content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if error != nil {
+                print(error.debugDescription)
+            }
+        }
+        
+        center.getPendingNotificationRequests { (request) in
+            print(request)
+        }
+    }
+    
     var body: some View {
-        Text("通知")
+        VStack {
+            Spacer()
+            Text("Notification Demo")
+            Spacer()
+            Button(action: {
+                self.setNotification()
+            }) {
+                Text("Set Notification!")
+            }
+            Spacer()
+        }
     }
 }
 
